5ead7df693259ee0f789700ac1171182e94d71ef
htmltest <struev.vs@gmail.com>
2016-04-04 14:43:28 +0300

    update 04.04.2016

--- js/tools.js
+++ js/tools.js
@@ -2,17 +2,17 @@
 
     $(document).ready(function() {
 
-        $('.form-input input').each(function() {
+        $('.form-input input, .form-textarea textarea').each(function() {
             if ($(this).val() == '') {
                 $(this).parent().find('span').css({'display': 'block'});
             }
         });
 
-        $('.form-input input').focus(function() {
+        $('.form-input input, .form-textarea textarea').focus(function() {
             $(this).parent().find('span').css({'display': 'none'});
         });
 
-        $('.form-input input').blur(function() {
+        $('.form-input input, .form-textarea textarea').blur(function() {
             if ($(this).val() == '') {
                 $(this).parent().find('span').css({'display': 'block'});
             }
@@ -22,6 +22,41 @@
         $('.form-checkbox').click(function() {
             $(this).toggleClass('checked');
             $(this).find('input').prop('checked', $(this).hasClass('checked')).trigger('change');
+        });
+
+        $('.add-form-media-radio input:checked').parent().addClass('checked');
+        var curIndex = $('.add-form-media-radio').index($('.add-form-media-radio.checked'));
+        $('.add-form-media-content').eq(curIndex).addClass('active');
+
+        $('.add-form-media-radio').click(function() {
+            var curName = $(this).find('input').attr('name');
+            $('.add-form-media-radio input[name="' + curName + '"]').parent().removeClass('checked');
+            $(this).addClass('checked');
+            $(this).find('input').prop('checked', true).trigger('change');
+
+            var curIndex = $('.add-form-media-radio').index($(this));
+
+            $('.add-form-media-content.active').removeClass('active');
+            $('.add-form-media-content').eq(curIndex).addClass('active');
+        });
+
+        $('.form-file input').change(function() {
+            var curFile = $(this).parent();
+            var options = {
+                success: function(data) {
+                    if (data.status == 1) {
+                        curFile.html('<img src="' + data.pic+ '" alt="" />')
+                    } else {
+                        alert(data.message, 'Ошибка');
+                    }
+                },
+                url:        '/ajax/newAvatar.php',
+                type:       'post',
+                dataType:   'json',
+                clearForm:  true,
+                resetForm:  true
+            };
+            $('input[name="newAva"]').parent().ajaxSubmit(options);
         });
 
         $('form').each(function() {
@@ -89,6 +124,21 @@
         $('.auth-forgot a').click(function(e) {
             $('.window').hide();
             $('.forgot').show();
+            e.preventDefault();
+        });
+
+        $('.main-item-link-new').click(function(e) {
+            $('.registration').show();
+            $('.overlay').show();
+            e.preventDefault();
+        });
+
+        $('.add-link').click(function(e) {
+            $('.add-form').show().css({'margin-top': -$('.add-form').height() / 2});
+            if ($('.add-form').offset().top < 0) {
+                $('.add-form').show().css({'margin-top': -$('.add-form').height() / 2 - $('.add-form').offset().top})
+            }
+            $('.overlay').show();
             e.preventDefault();
         });
 
